6d667601da86e955bd8d866fba46e9c295d17c51
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-24 17:12:04 +0300

    01-color-switcher final

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+let timerId = null;
+
+refs.start.addEventListener('click', changeBkcolor);
+
+refs.stop.addEventListener('click', () => {
+    clearInterval(timerId);
+    enableButton();
+});
+
+
+
+
+function changeBkcolor() {
+    timerId = setInterval(() => {
+        const hexColor = getRandomHexColor();
+        refs.body.style.backgroundColor = hexColor;
+    }, 1000);
+
+    disabledButton();
+};
+
+function disabledButton() {
+    refs.start.disabled = true;
+}
+
+function enableButton() {
+    refs.start.disabled = false;
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
