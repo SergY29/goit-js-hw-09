adf08023ca10e1ac71ce999298e0f12dbb4f7c4e
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-25 17:18:20 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,6 +7,7 @@ const refs = {
 };
 
 const defaultDate = Date.now();
+const TIME = "time";
 const options = {
     enableTime: true,
     dateFormat: "Y-m-d H:i",
@@ -14,41 +15,33 @@ const options = {
     defaultDate: Date.now(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
-        const secondTime = selectedDates[0];
+        // console.log(selectedDates[0]);
+        const value = (selectedDates[0] - defaultDate);
         if ((defaultDate - selectedDates[0]) < 86400000) {
             enableButton();
+            localStorage.setItem(TIME, value);
         };
     },
     onChange(selectedDates) {
         if ((defaultDate - selectedDates[0]) > 86400000) {
             window.alert("Please choose a date in the future");
             disabledButton();
-            console.log(options.onClose.secondTime);
         }
     },
 };
 
-
-
-
 flatpickr(refs.inputTime, options);
-
 disabledButton();
-
-
 
 refs.start.addEventListener('click', start);
 
-
 function start() {
     setInterval(() => {
-        console.log('начался отсчет');
+        const referenceTime = localStorage.getItem(TIME);
+        // convertMs(ms);
+        console.log(referenceTime);
     }, 1000);
 }
-
-
-
 
 function disabledButton() {
     refs.start.disabled = true;
